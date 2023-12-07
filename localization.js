6953386aa834c2d2aa3a26dcf38f57baffc6345f
Vlad Shalya <61494265+CoringPlay@users.noreply.github.com>
2023-12-07 12:27:35 +1000

    Create localization.js

---
+++ localization.js
@@ -0,0 +1,7 @@
+const translations = {
+        'Community Center': 'Общественный Центр',
+        'Special Requests': 'Особые запросы',
+        'Selected Seeds': 'Отборные семена',
+        'Back': 'Назад',
+};
+export default translations;
