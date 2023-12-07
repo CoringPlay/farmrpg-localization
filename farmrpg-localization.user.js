a1c2bed365ea3f630e2cdecfec412e1d9456bc24
Vlad Shalya <61494265+CoringPlay@users.noreply.github.com>
2023-12-07 12:40:30 +1000

    Update farmrpg-localization.user.js

--- farmrpg-localization.user.js
+++ farmrpg-localization.user.js
@@ -2,47 +2,60 @@
 // @name         FarmRPG Localizer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Localize FarmRPG website
+// @description  Localize FarmRPG website without language selection
 // @author       You
 // @match        https://farmrpg.com/*
 // @grant        none
 // ==/UserScript==
 
-// Импортируем основной скрипт
-const script = document.createElement('script');
-script.src = 'https://raw.githubusercontent.com/CoringPlay/farmrpg-localization/main/localization.js';
+(async function() {
+    'use strict';
 
-function replaceText(node) {
-    node.nodeValue = node.nodeValue.replace(
-        new RegExp(Object.keys(translations).join('|'), 'g'),
-        matched => translations[matched]
-    );
-}
-function traverse(node) {
-    const treeWalker = document.createTreeWalker(
-        node,
-        NodeFilter.SHOW_TEXT,
-        null,
-        false
-    );
-    while (treeWalker.nextNode()) {
-        replaceText(treeWalker.currentNode);
+    // Загрузка файла с переводами
+    const response = await fetch('https://raw.githubusercontent.com/CoringPlay/farmrpg-localization/main/localization.js');
+    const translations = await response.json();
+
+    function replaceText(node) {
+        node.nodeValue = node.nodeValue.replace(
+            new RegExp(Object.keys(translations).join('|'), 'g'),
+            matched => translations[matched]
+        );
     }
-}
-function handleMutations(mutationsList) {
-    for (const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            mutation.addedNodes.forEach(node => {
-                traverse(node);
-            });
+
+    function traverse(node) {
+        const treeWalker = document.createTreeWalker(
+            node,
+            NodeFilter.SHOW_TEXT,
+            null,
+            false
+        );
+
+        while (treeWalker.nextNode()) {
+            replaceText(treeWalker.currentNode);
         }
     }
-}
-const observer = new MutationObserver(handleMutations);
-const observerConfig = {
-    childList: true,
-    subtree: true,
-    characterData: true
-};
-traverse(document.body);
-observer.observe(document.body, observerConfig);
+
+    function handleMutations(mutationsList) {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                mutation.addedNodes.forEach(node => {
+                    traverse(node);
+                });
+            }
+        }
+    }
+
+    const observer = new MutationObserver(handleMutations);
+
+    const observerConfig = {
+        childList: true,
+        subtree: true,
+        characterData: true
+    };
+
+    // Применяем локализацию при загрузке
+    traverse(document.body);
+
+    // Начинаем отслеживание изменений
+    observer.observe(document.body, observerConfig);
+})();
